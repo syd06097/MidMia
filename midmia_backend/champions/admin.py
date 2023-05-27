6cca22b6247f9297962e7b12d0ad921c0fd27a54
syd06097 <syd060978@gmail.com>
2023-05-28 01:56:24 +0900

    fixed untracked files

--- midmia_backend/champions/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Champion, ChampionStats
-
-admin.site.register(Champion)
-admin.site.register(ChampionStats)
-
-# Register your models here.
--- midmia_backend/summoners/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'summoners'
-
-urlpatterns = [
-    path('ranking/', views.summoner_ranking, name='ranking'),
-]
