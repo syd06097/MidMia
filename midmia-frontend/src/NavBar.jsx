7da5bbd55d277dadd55c3ae8567ce33d6c7cd178
slow9424 <134119595+slow9424@users.noreply.github.com>
2023-06-14 13:13:58 +0900

    NAV

--- midmia-frontend/src/NavBar.jsx
+++ midmia-frontend/src/NavBar.jsx
@@ -10,12 +10,14 @@ function NavBar() {
         <Container>
           <Link to="/" className="navbar-brand"><h3><b>MidMia</b></h3></Link>
           <Nav className="me-auto">
+          <Link to="/" className="nav-link" style={{ marginRight: 25, marginLeft: 25 }}>챔피언</Link>
             <Link to="/ChampionState" className="nav-link" style={{ marginRight: 25, marginLeft: 25 }}>챔피언 통계</Link>
-            <Nav.Link href="#pricing" style={{ marginRight: 25, marginLeft: 25 }}>소환사 랭킹</Nav.Link>
-            <Nav.Link href="#a" style={{ marginRight: 25, marginLeft: 25 }}>커뮤니티</Nav.Link>
+            <Link to="/Summoner" className="nav-link" style={{ marginRight: 25, marginLeft: 25 }}>소환사 랭킹</Link>{/*Summoner이건 왜 안되는거야 */}
+            <Link to="/" className="nav-link" style={{ marginRight: 25, marginLeft: 25 }}>커뮤니티</Link>
           </Nav>
           <Nav className="ml-auto">
-            <Nav.Link href="#b" style={{ marginRight: 25, marginLeft: 25 }}>로그인</Nav.Link>
+            <Link to="" className="nav-link" style={{ marginRight: 25, marginLeft: 25 }}>로그인</Link>
+            {/* <Nav.Link href="#b" style={{ marginRight: 25, marginLeft: 25 }}>로그인</Nav.Link>*/}
           </Nav>
         </Container>
       </Navbar>
