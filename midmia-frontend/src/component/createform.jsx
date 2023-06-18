7c3cc064eb4d07f59c57ded0a79c17813bca3902
syd06097 <syd060978@gmail.com>
2023-06-18 15:47:09 +0900

    커뮤니티
    .

---
+++ midmia-frontend/src/component/createform.jsx
@@ -0,0 +1,122 @@
+import React, { useState, useEffect} from "react";
+import axios from 'axios';
+
+function CreatePostForm() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [category, setCategory] = useState("");
+  const [subcategory, setSubcategory] = useState("");
+  
+
+
+  const handleCategoryChange = (e) => {
+    setCategory(e.target.value);
+    // 선택된 카테고리에 따라 하위 카테고리(subcategory) 업데이트 로직 구현
+    if (e.target.value === "position") {
+      setSubcategory("top"); // 예시: 기본 하위 카테고리 값을 "top"으로 설정
+    } else if (e.target.value === "free") {
+      setSubcategory("humor"); // 예시: 기본 하위 카테고리 값을 "humor"으로 설정
+    }
+  };
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("content", content);
+    formData.append("category", category);
+    formData.append("subcategory", subcategory);
+
+    for (let pair of formData.entries()) {
+      console.log(pair[0] + ": " + pair[1]);
+    }
+    
+    axios.post("http://127.0.0.1:8000/community/login/", {
+      username: "test",
+      password: "1q2w3e4r!"
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          // 로그인이 성공적으로 완료된 경우에 대한 처리
+          axios.defaults.headers.common['Authorization'] = `Token ${response.data.token}`;
+          return axios.post("http://127.0.0.1:8000/community/create/", formData, {
+            'Content-Type': 'multipart/form-data',
+          });
+        } else {
+          // 로그인이 실패한 경우에 대한 처리
+          console.error("로그인 실패");
+          throw new Error("로그인 실패");
+        }
+      })
+      .then((response) => {
+        if (response.status === 201) {
+          // 글 작성이 성공적으로 완료된 경우에 대한 처리
+          console.log("글 작성이 성공적으로 완료되었습니다.");
+          // 필요한 리다이렉션 또는 다음 동작을 수행
+        } else {
+          // 글 작성이 실패한 경우에 대한 처리
+          console.error("글 작성 중 오류가 발생했습니다.");
+        }
+      })
+      .catch((error) => {
+        console.error("네트워크 오류:", error);
+      });
+  };
+
+  return (
+    <div>
+      <h1>Create Post</h1>
+      <form onSubmit={handleSubmit}>
+        <br />
+        <label>
+          Category:
+          <select value={category} onChange={handleCategoryChange}>
+            <option value="">Select Category</option>
+            <option value="position">포지션 게시판</option>
+            <option value="free">자유 게시판</option>
+          </select>
+        </label>
+        <br />
+        {category === "position" && (
+          <label>
+            Subcategory:
+            <select value={subcategory} onChange={(e) => setSubcategory(e.target.value)}>
+              <option value="">Select Subcategory</option>
+              <option value="top">탑</option>
+              <option value="jungle">정글</option>
+              <option value="mid">미드</option>
+              <option value="bottom">바텀</option>
+              <option value="support">서폿</option>
+            </select>
+          </label>
+        )}
+        {category === "free" && (
+          <label>
+            Subcategory:
+            <select value={subcategory} onChange={(e) => setSubcategory(e.target.value)}>
+              <option value="">Select Subcategory</option>
+              <option value="humor">유머</option>
+              <option value="freef">자유</option>
+            </select>
+          </label>
+        )}
+        <br />
+        <label>
+          Title:
+          <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Content:
+          <textarea value={content} onChange={(e) => setContent(e.target.value)} />
+        </label>
+        
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreatePostForm;
---
+++ midmia-frontend/src/pages/CreatePost.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import ListGroup from 'react-bootstrap/ListGroup';
+import NavBar from '../NavBar';//상단 메뉴바
+import MenuBackimage from '../MenuBackimage';//배경
+import CreatePostForm from "../component/createform";
+
+
+const mainStyle = {
+  backgroundColor: 'rgb(033, 033, 033)', // 배경이미지가 없는 공간의 배경색
+  minHeight: '100vh', // 화면 전체 높이로 설정
+};//회색배경
+
+const pagebottom = {
+  borderTop: '1px solid rgb(100,100,100)',
+  width: '70%',
+  display: 'inline-block',
+  transform: 'translate(15%,0%)',
+  margin: '100px 50px',
+  padding: '20px'
+}//아래 추가용 스타일
+
+
+
+function CreatePost() {
+
+  return (
+    <div>
+      <div style={mainStyle}>
+        <NavBar />{/* 상단 메뉴바 */}
+        <MenuBackimage name="글작성" />{/* 상단의 이미지 배경 */}
+        <CreatePostForm/>
+        <ListGroup style={pagebottom}>
+          <small style={{ color: 'white' }}>© 2023-2023 MidMia  isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends.<br />
+            League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.</small>
+        </ListGroup>
+      </div>
+    </div>
+  );
+}
+
+export default CreatePost;
