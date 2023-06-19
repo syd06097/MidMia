f8388cd21495d346800359ee8be6a7b123cddc2b
yhd1101 <117626705+yhd1101@users.noreply.github.com>
2023-06-19 23:33:39 +0900

    commits

--- midmia-frontend/src/ImageSlider.jsx
+++ midmia-frontend/src/ImageSlider.jsx
@@ -4,13 +4,13 @@ import 'swiper/swiper.min.css';
 import { Card, Button } from 'react-bootstrap';
 
 const cardData = [
-  { id: 1, title: 'Card 1', text: 'Text 1', image: './dog.jpg' },
-  { id: 2, title: 'Card 2', text: 'Text 2', image: './dog.jpg' },
-  { id: 3, title: 'Card 3', text: 'Text 3', image: './dog.jpg' },
-  { id: 4, title: 'Card 4', text: 'Text 3', image: './dog.jpg' },
-  { id: 5, title: 'Card 5', text: 'Text 3', image: './dog.jpg' },
-  { id: 6, title: 'Card 6', text: 'Text 3', image: './dog.jpg' },
-  { id: 7, title: 'Card 7', text: 'Text 3', image: './dog.jpg' },
+  { id: 1, title: 'Card 1', text: 'Text 1', image: 'https://media.discordapp.net/attachments/1037323910087180321/1120326462608777226/1.png?width=266&height=446' },
+  { id: 2, title: 'Card 2', text: 'Text 2', image: 'https://media.discordapp.net/attachments/1037323910087180321/1120326462847864882/1.png?width=267&height=446' },
+  { id: 3, title: 'Card 3', text: 'Text 3', image: 'https://media.discordapp.net/attachments/1037323910087180321/1120326463057563679/1.png?width=267&height=446' },
+  { id: 4, title: 'Card 4', text: 'Text 3', image: 'https://media.discordapp.net/attachments/1037323910087180321/1120326463279865956/1.png?width=267&height=446' },
+  { id: 5, title: 'Card 5', text: 'Text 3', image: 'https://media.discordapp.net/attachments/1037323910087180321/1120326463556702218/1.png?width=267&height=446' },
+  { id: 6, title: 'Card 6', text: 'Text 3', image: 'https://media.discordapp.net/attachments/1037323910087180321/1120326463883841536/1.png?width=267&height=446' },
+  { id: 7, title: 'Card 7', text: 'Text 3', image: 'https://media.discordapp.net/attachments/1037323910087180321/1120326464420728972/1.png?width=267&height=446' },
   // 추가적인 카드 데이터
 ];
 
@@ -24,11 +24,10 @@ function ImageSlider() {
     <Swiper {...swiperParams}>
       {cardData.map((card) => (
         <SwiperSlide key={card.id}>
-          <Card style={{ width: '13rem', color:'white' , textAlign:'center'}}>
-            <Card.Img variant="top" src="dog.jpg" style={{objectFit: 'cover', height: '100%' }}/>
-            <Card.Body style={{backgroundColor: 'rgb(033, 033, 033)'}}>
-              <Card.Title style={{ height: '1rem' }}>{<h4>{card.title}</h4>}</Card.Title> 
-            </Card.Body>
+          <Card style={{ width: '15rem', color: 'white', textAlign: 'center', borderRadius: 0 }}>
+            <Card.Img variant="top" src={card.image} style={{objectFit: 'cover', height: '100%', width: '100%' }}/>
+            {/* <Card.Body style={{backgroundColor: 'rgb(033, 033, 033)'}}>
+            </Card.Body> */}
           </Card>
         </SwiperSlide>
       ))}
--- midmia-frontend/src/MainCommunity.jsx
+++ midmia-frontend/src/MainCommunity.jsx
@@ -1,15 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import styles from "./css/MainCommunity.module.css";//모듈 붙으면 styles 붙여서 사용
-
+import axios from 'axios';
+import PostList from './component/postlist';
 //import './css/MainCommunity.css'; 
 
 function MainCommunity() {
   const [data, setData] = useState([]); // 서버에서 받아온 데이터를 저장할 상태
 
+  // const formatDate = (dateString) => {
+  //   const date = new Date(dateString);
+  //   const year = date.getFullYear();
+  //   const month = String(date.getMonth() + 1).padStart(2, '0');
+  //   const day = String(date.getDate()).padStart(2, '0');
+    
+  //   return `${year}-${month}-${day}`;
+  // };
+
+  
+  // useEffect(() => {
+   
+    // API 요청을 통해 게시글 목록을 가져옵니다.
+    // axios.get('http://127.0.0.1:8000/community/')
+    //   .then(response => {
+    //     // API 요청이 성공하면 가져온 데이터를 처리합니다.
+    //     const posts = response.data;
+    //     let filteredPosts = [];
+        
+    //     if (category === 'position_all') {
+    //       filteredPosts = posts.filter(item =>
+    //         ["top", "jungle", "mid", "bottom", "support"].includes(item.subcategory)
+    //       );
+    //     } else if (category === 'free_all') {
+    //       filteredPosts = posts.filter(item =>
+    //         ["free", "humor"].includes(item.subcategory)
+    //       );
+    //     } else {
+    //       filteredPosts = posts.filter(item => item.subcategory === category)
+    //     }
+    //     setDataList(filteredPosts);
+    //   })
+    //   .catch(error => {
+    //     // API 요청이 실패한 경우 에러 처리를 수행합니다.
+    //     console.error(error);
+    //   });
+  // }, []);
+
+
   useEffect(() => {
     // 서버에서 데이터를 가져오는 비동기 함수 호출
     fetchData();
+    // getCommunity()
   }, []);
 
   const fetchData = async () => {
@@ -18,6 +59,9 @@ function MainCommunity() {
       // 예시: const response = await fetch('서버 API URL');
       //       const data = await response.json();
       //       setData(data); // 가져온 데이터를 상태에 저장
+      const result = await axios.get('http://127.0.0.1:8000/community/')
+      setData(result.data)
+
     } catch (error) {
       console.log(error);
     }
@@ -26,33 +70,44 @@ function MainCommunity() {
 
     <div>
       <div className={styles.div0} >{/* 나중에 위치조정필요 */}
-        <ListGroup>
-          <ListGroup.Item className={styles.write} style={{ marginTop: '20px' }}><h3 className={styles.title}>포지션 게시판</h3></ListGroup.Item>
-          <ListGroup.Item className={styles.write}>요즘 밸런스 이상함</ListGroup.Item>
-          <ListGroup.Item className={styles.write}>원딜 죽어간다</ListGroup.Item>
-          <ListGroup.Item className={styles.write}>서폿들 요즘 기어오르면 개추</ListGroup.Item>
-          <ListGroup.Item className={styles.write}>정글 뭐하냐고</ListGroup.Item>
-          <ListGroup.Item className={styles.write}>정글 뭐하냐고</ListGroup.Item>
-          <ListGroup.Item className={styles.write}>정글 뭐하냐고</ListGroup.Item>
-          <ListGroup.Item className={styles.write}>정글 뭐하냐고</ListGroup.Item>
-          <ListGroup.Item className={styles.write}>서폿들 요즘 기어오르면 개추</ListGroup.Item>
+      <h3 className={styles.title}>포지션게시판</h3>
+
+      {data.map(d => (
+        <ListGroup key={d.id}>
+          <ListGroup.Item className={styles.write}>{d.title}</ListGroup.Item>
         </ListGroup>
+      ))}
+     
+        {/* {data && data.map(d => {
+           <ListGroup key={d.id}>
+            <ListGroup.Item className={styles.write} style={{ marginTop: '20px' }}><h3 className={styles.title}>포지션 게시판</h3></ListGroup.Item>
+            <ListGroup.Item className={styles.write}>{d.title}</ListGroup.Item>
+          </ListGroup>
+
+
+        })} */}
+      
+     
+      
+    
       </div>
       <div className={styles.div0}>{/* 나중에 위치조정필요 */}
-        <ListGroup>
-          <ListGroup.Item className={styles.write} style={{ marginTop: '20px' }}><h3 className={styles.title}>자유 게시판</h3></ListGroup.Item>
-          <ListGroup.Item className={styles.write}>요즘 밸런스 이상함</ListGroup.Item>
-          <ListGroup.Item className={styles.write}>원딜 죽어간다</ListGroup.Item>
-          <ListGroup.Item className={styles.write}>서폿들 요즘 기어오르면 개추</ListGroup.Item>
-          <ListGroup.Item className={styles.write}>정글 뭐하냐고</ListGroup.Item>
-          <ListGroup.Item className={styles.write}>정글 뭐하냐고</ListGroup.Item>
-          <ListGroup.Item className={styles.write}>정글 뭐하냐고</ListGroup.Item>
-          <ListGroup.Item className={styles.write}>정글 뭐하냐고</ListGroup.Item>
-          <ListGroup.Item className={styles.write}>서폿들 요즘 기어오르면 개추</ListGroup.Item>
+      <h3 className={styles.title}>자유게시판</h3>
+      {data.map(d => (
+        <ListGroup key={d.id}>
+          <ListGroup.Item className={styles.write}>{d.title}</ListGroup.Item>
         </ListGroup>
+      ))}
+      {/* {data && data.map(d => {
+        <ListGroup key={d.id}>
+         <ListGroup.Item className={styles.write} style={{ marginTop: '20px' }}><h3 className={styles.title}>포지션 게시판</h3></ListGroup.Item>
+         <ListGroup.Item className={styles.write}>ssss</ListGroup.Item>
+       </ListGroup>
+
+      })} */}
       </div>
     </div>
   );
 }
 
-export default MainCommunity;
+export default MainCommunity
